98a98d138c4432bc0e779dfe9186c1585ba583df
Ralf Waspe <rwaspe@icloud.com>
2022-04-01 10:58:15 +0200

    fixed processor

--- tools/find_qt.py
+++ tools/find_qt.py
@@ -27,19 +27,17 @@ pathDict = {
     }
 }
 
-print(platform.machine())
-
 if not platform.system() in pathDict:
-    print('system', platform.system())
+    #print('system', platform.system())
     sys.exit(1)
 
 archDict = pathDict[platform.system()]
 
 if not platform.machine() in archDict:
-    print('machine',platform.machine())
+    #print('machine',platform.machine())
     sys.exit(1)
 
 path = archDict[platform.machine()]
 qtPath += path + '/'
 
-print('path', qtPath)
+print(qtPath)
